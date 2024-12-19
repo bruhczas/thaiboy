acbda95c2e78a5fc2d3fdca2f1ee567189feafe2
bruhczas <71846288+bruhczas@users.noreply.github.com>
2024-12-19 17:45:58 +0100

    Update prawdziwyserwer.js

--- prawdziwyserwer.js
+++ prawdziwyserwer.js
@@ -4,13 +4,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Serwowanie plików statycznych
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Domyślny endpoint dla frontendu
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Uruchomienie serwera
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
